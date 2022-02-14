f62f465d3d8fefe4c1b1efbe6a0ed06f6c6d20be
Gozzissimo <fantini.nico@gmail.com>
2022-02-14 22:35:35 +0100

    header

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,9 +1,40 @@
 <header>
-    <nav>
-        <ul>
+    <nav class="container flex">
+        <div class="logo">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+            </a>
+        </div>
+        <ul class="flex">
             <li>
-                <a href="{{ route('home') }}"><img src="{{ asset('img/dc-logo.png') }}" alt="logo"></a>
-                <a href="{{ route('comics') }}">COMICS</a>
+                <a href="#">characters</a>
+            </li>
+            <li>
+                <a href="{{ route('comics') }}">comics</a>
+            </li>
+            <li>
+                <a href="#">movies</a>
+            </li>
+            <li>
+                <a href="#">tv</a>
+            </li>
+            <li>
+                <a href="#">games</a>
+            </li>
+            <li>
+                <a href="#">collectibles</a>
+            </li>
+            <li>
+                <a href="#">videos</a>
+            </li>
+            <li>
+                <a href="#">fans</a>
+            </li>
+            <li>
+                <a href="#">news</a>
+            </li>
+            <li>
+                <a href="#">shop</a>
             </li>
         </ul>
     </nav>

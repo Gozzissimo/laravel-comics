fe0259f0be88217c14ea15f2a75783801bf3c9fe
Gozzissimo <fantini.nico@gmail.com>
2022-02-14 23:19:25 +0100

    Comics Jumbotron and containers

--- resources/views/guest/comics.blade.php
+++ resources/views/guest/comics.blade.php
@@ -5,5 +5,59 @@
 @endsection
 
 @section('content')
-    Tutti i fumetti
+    <main class="comics">
+        <section class="main-top">
+            <div class="jumbotron">
+                <h1>
+                    CURRENT SERIES
+                </h1>
+            </div>
+            <div class="container">
+                <div class="comics flex">
+                <!-- QUI FOREACH -->
+                </div>
+                <div class="button">
+                    <a class="load-more" href="#">
+                        LOAD MORE
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="main-bottom">
+            <div class="container">
+                <ul class="flex">
+                    <li class="flex">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+                        <a class="flex" href="#">
+                            digital comics
+                        </a>
+                    </li>
+                    <li class="flex">
+                        <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="">
+                        <a class="flex" href="#">
+                            dc merchandise
+                        </a>
+                    </li>
+                    <li class="flex">
+                        <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="">
+                        <a class="flex" href="#">
+                            subscription
+                        </a>
+                    </li>
+                    <li class="flex">
+                        <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="">
+                        <a class="flex" href="#">
+                            comic shop locator
+                        </a>
+                    </li>
+                    <li class="flex">
+                        <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="">
+                        <a class="flex" href="#">
+                            dc power visa
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
 @endsection

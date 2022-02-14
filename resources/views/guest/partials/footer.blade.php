7ac95ab65e4ba61ec19b4b14426a53887898c2ec
Gozzissimo <fantini.nico@gmail.com>
2022-02-14 22:55:27 +0100

    footer

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,3 +1,70 @@
 <footer>
-    footer
+    <div class="footer-top">
+        <div class="container flex">
+            <div class="col">
+                <h4>dc comics</h4>
+                <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+                <h4>shop</h4>
+                <ul>
+                    <li><a href="#">Lorem Ipsum lungo</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>dc</h4>
+                <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>sites</h4>
+                <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container flex">
+            <div class="side-left">
+                <a class="cta" href="#">SIGN-UP NOW!</a>
+            </div>
+            <div class="side-right flex">
+                <h3>FOLLOW US</h3>
+                <ul class="social flex">
+                    <li>
+                        <a href="#"><img src="{{ asset('img/footer-facebook.png') }}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="{{ asset('img/footer-twitter.png') }}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="{{ asset('img/footer-youtube.png') }}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="{{ asset('img/footer-pinterest.png') }}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="{{ asset('img/footer-periscope.png') }}" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
